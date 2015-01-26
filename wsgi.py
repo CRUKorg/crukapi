755cd348489530ecfb5bb24de9d8d31cc1e7ca45
Matthew Griffiths <mrmatthewgriffiths@gmail.com>
2015-01-26 18:32:34 +0000

    CI

--- wsgi.py
+++ wsgi.py
@@ -7,9 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 import sys
-sys.path.append(os.path.dirname(__file__))
-
 import os
+
+sys.path.append(os.path.dirname(__file__))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
