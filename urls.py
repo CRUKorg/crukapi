d542e18195ca7f504c5500175fc987b0537c6fc9
Matthew Griffiths <mrmatthewgriffiths@gmail.com>
2015-01-29 10:23:53 +0000

    Added heart beat

--- urls.py
+++ urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.decorators.http import require_http_methods
 from django.views.defaults import permission_denied
+from jsonview.decorators import json_view
+
+
+@require_http_methods(["GET"])
+@json_view
+def heartbeat(request):
+    return {'status': 'ok'}
+
 
 urlpatterns = patterns(
     '',
@@ -13,5 +22,8 @@ urlpatterns = patterns(
     # Urls for the main portal
     url(r'^api', include("apis.urls")),
 
-    url(r'', permission_denied)
+    # Urls for LB heath check
+    url(r'^heartbeat', heartbeat, name="heartbeat"),
+
+    url(r'', permission_denied),
 )
