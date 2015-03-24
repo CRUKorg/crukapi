cd9c18f3ead6e88fc8aa9e84fc783474a79eefec
Matthew Griffiths <mrmatthewgriffiths@gmail.com>
2015-03-24 14:44:59 +0000

    Added basic dashboard visualizations

--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,7 @@
+import json
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from apis.models import RequestLogV1
@@ -18,9 +20,22 @@ def portal_home(request):
 @login_required()
 def api_theimpossibleline(request):
     requests = RequestLogV1.objects.filter(project="theimpossibleline")
+    requests_by_day = requests.extra(select={'day': 'date(created)'}).values('day').annotate(count=Count('created'))
+    request_chart_data = [['Day', 'Classifications']]
+    for classification_by_day in requests_by_day:
+        request_chart_data.append(["%s" % str(classification_by_day['day']), classification_by_day['count']])
+
+    registration_requests = requests.filter(path=reverse("api_projects_impossible_line_signup"))
+    registrations_by_day = requests.extra(select={'day': 'date(created)'}).values('day').annotate(count=Count('created'))
+    registrations_chart_data = [['Day', 'Classifications']]
+    for classification_by_day in registrations_by_day:
+        registrations_chart_data.append(["%s" % str(classification_by_day['day']), classification_by_day['count']])
+
     template_data = {
-        'total_registrations': requests.filter(path=reverse("api_projects_impossible_line_signup")).count(),
-        'total_api_requests': requests.count()}
+        'total_registrations': registration_requests.count(),
+        'total_api_requests': requests.count(),
+        'chart_requests_over_time': json.dumps(request_chart_data),
+        'chart_registrations_over_time': json.dumps(registrations_chart_data)}
     return render(request, 'portal/pages/apis/theimpossibleline.html', template_data)
 
 
